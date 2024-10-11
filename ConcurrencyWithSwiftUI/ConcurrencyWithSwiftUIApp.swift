6af0ca40c1fe26248def1c2d6b0790d36ea262b4
rafiulhasanarb <rafiulhasanarb@gmail.com>
2024-10-11 12:37:17 +0600

    Initial Commit

---
+++ ConcurrencyWithSwiftUI/ConcurrencyWithSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ConcurrencyWithSwiftUIApp.swift
+//  ConcurrencyWithSwiftUI
+//
+//  Created by Rafiul Hasan on 10/11/24.
+//
+
+import SwiftUI
+
+@main
+struct ConcurrencyWithSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ConcurrencyWithSwiftUITests/ConcurrencyWithSwiftUITests.swift
@@ -0,0 +1,16 @@
+//
+//  ConcurrencyWithSwiftUITests.swift
+//  ConcurrencyWithSwiftUITests
+//
+//  Created by Rafiul Hasan on 10/11/24.
+//
+
+import Testing
+
+struct ConcurrencyWithSwiftUITests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
